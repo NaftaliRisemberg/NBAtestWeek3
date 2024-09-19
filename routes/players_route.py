8babd50d113841372ddc2e9b0cd64e5c1fcef24b
NaftaliRisemberg <n9742686@gmail.com>
2024-09-19 17:59:10 +0300

    start working in the routes and finished time

--- routes/players_route.py
+++ routes/players_route.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, jsonify, request
+from services.player_service import *
 from models.player import Player
-from db import db
+from sqlalchemy import and_
+
 
 player_bp = Blueprint('player', __name__, url_prefix='/api/players')
 
-@player_bp.route('?position={position}&season={season} ', methods=['GET'])
+@player_bp.route('position/<string:position>/<int:season>', methods=['GET'])
 def get_players(position, season):
-    pass
+    players = Player.query.all()
+    data = list(filter(lambda player: player.position == position and
+                                      player.season == season, players))
+    players_list = [player.get_dict() for player in data]
+    return jsonify(players_list)
---
+++ services/player_service.py
@@ -0,0 +1,9 @@
+from models.player import Player
+from db import db
+
+
+def get_players_by_position(position):
+    pass
+
+def get_player_by_season(season):
+    pass
